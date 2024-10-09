d5b9b0d58acfdfbd6f7ba6257e22714f7d37591c
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-10-09 18:50:08 +0530

    Create order service

--- pos-spring-backend/src/main/java/lk/ijse/posspringbackend/service/OrderService.java
+++ pos-spring-backend/src/main/java/lk/ijse/posspringbackend/service/OrderService.java
@@ -1,5 +1,15 @@
 package lk.ijse.posspringbackend.service;
 
-public interface OrderService {
+import lk.ijse.posspringbackend.dto.OrderStatus;
+import lk.ijse.posspringbackend.dto.impl.OrderDTO;
 
+import java.util.List;
+
+public interface OrderService {
+    String generateNextOrderId();
+    void saveOrder(OrderDTO orderDTO);
+    OrderStatus getOrder(String orderId);
+    void deleteOrder(String orderId);
+    List<OrderDTO> getAllOrders();
+    void updateOrder(String orderId, OrderDTO orderDTO);
 }
