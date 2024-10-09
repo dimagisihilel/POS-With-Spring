d4edd88bbd6409205813face4984809bb9cf4f52
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-10-09 17:16:03 +0530

    Add Regex for ids

---
+++ pos-spring-backend/src/main/java/lk/ijse/posspringbackend/util/RegexUtil.java
@@ -0,0 +1,17 @@
+package lk.ijse.posspringbackend.util;
+
+public class RegexUtil {
+
+    // Regex pattern for Customer ID
+    public static final String CUSTOMER_ID_REGEX = "^[a-zA-Z0-9]+$";
+
+    // Regex pattern for Item ID
+    public static final String ITEM_ID_REGEX = "^[a-zA-Z0-9]+$";
+
+    // Regex pattern for Order ID
+    public static final String ORDER_ID_REGEX = "^[a-zA-Z0-9]+$";
+
+    // Regex pattern for Order Detail ID
+    public static final String ORDER_DETAIL_ID_REGEX = "^[a-zA-Z0-9]+$";
+
+}
