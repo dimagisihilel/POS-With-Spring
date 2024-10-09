788a9ac168803cd6f3fbf786c121008752419fb7
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-10-09 19:11:10 +0530

    Add order detail service

--- pos-spring-backend/src/main/java/lk/ijse/posspringbackend/service/OrderDetailService.java
+++ pos-spring-backend/src/main/java/lk/ijse/posspringbackend/service/OrderDetailService.java
@@ -1,6 +1,8 @@
 package lk.ijse.posspringbackend.service;
 
+import lk.ijse.posspringbackend.dto.impl.OrderDetailDTO;
 
 public interface OrderDetailService {
+    void saveOrderDetail(OrderDetailDTO orderDetailDTO);
 
 }
