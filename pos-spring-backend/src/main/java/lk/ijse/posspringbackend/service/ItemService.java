6871588580a59cebd0ef134f20b53b2595a6c409
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-10-09 18:15:51 +0530

    Add methods Item service

--- pos-spring-backend/src/main/java/lk/ijse/posspringbackend/service/ItemService.java
+++ pos-spring-backend/src/main/java/lk/ijse/posspringbackend/service/ItemService.java
@@ -1,5 +1,15 @@
 package lk.ijse.posspringbackend.service;
 
+import lk.ijse.posspringbackend.dto.ItemStatus;
+import lk.ijse.posspringbackend.dto.impl.ItemDTO;
+
+import java.util.List;
+
 public interface ItemService {
+    void saveItem(ItemDTO itemDTO);
+    ItemStatus getItem(String itemId);
+    void deleteItem(String itemId);
+    List<ItemDTO> getAllItems();
+    void updateItem(String itemId, ItemDTO itemDTO);
 
 }
