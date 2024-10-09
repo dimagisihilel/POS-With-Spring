afebd9e2993e68fc9d3027f082626d5847db33d8
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-10-09 16:44:43 +0530

    Add Customer entity

---
+++ pos-spring-backend/src/main/java/lk/ijse/posspringbackend/entity/impl/CustomerEntity.java
@@ -0,0 +1,24 @@
+package lk.ijse.posspringbackend.entity.impl;
+
+
+import jakarta.persistence.*;
+import lk.ijse.posspringbackend.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerEntity implements SuperEntity {
+    @Id
+    private String customerId;
+    private String name;
+    private String address;
+    private String contact;
+
+}
